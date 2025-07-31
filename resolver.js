e2a74c136a2a0ca1c0ac8c7cdc6dfdbcd6920bd5
Vijay Mathew <vijay@fractl.io>
2025-07-31 16:04:06 +0530

    support suspension-restart

--- resolver.js
+++ resolver.js
@@ -6,15 +6,17 @@ function getUrl(endpoint) {
     return SlackBaseUrl + "/" + endpoint
 }
 
-const StandardHeaders = {"Authorization": "Bearer " + SlackApiKey,
-			 "Content-Type": "application/json"}
+const StandardHeaders = {
+    "Authorization": "Bearer " + SlackApiKey,
+    "Content-Type": "application/json"
+}
 
 async function handleFetch(url, req) {
     try {
         const response = await fetch(url, req);
         if (!response.ok) {
-            return {error: `HTTP error! status: ${response.status} ${response.text} ${response.statusText}`}
-	}
+            return { error: `HTTP error! status: ${response.status} ${response.text} ${response.statusText}` }
+        }
         return await response.json();
     } catch (error) {
         return { error: error.message };
@@ -30,11 +32,13 @@ async function waitForReply(thread) {
     });
     const msgs = resp['messages']
     if (msgs.length >= 2) {
-	return msgs[msgs.length - 1]['text']
+        return msgs[msgs.length - 1]['text']
     } else {
-	return 'no response'
+        return 'no response'
     }
 }
+
+const AL_HOST = process.env['AL_HOST'] || 'http://localhost:8080'
 
 export async function send(channel, message, env) {
     const apiUrl = getUrl("chat.postMessage")
@@ -42,14 +46,23 @@ export async function send(channel, message, env) {
     const r = await handleFetch(apiUrl, {
         method: 'POST',
         headers: StandardHeaders,
-	body: JSON.stringify({channel: SlackChannelId,
-			      text: `${suspId} -- ${message}`,
-			      mrkdwn: true})
+        body: JSON.stringify({
+            channel: SlackChannelId,
+            markdown_text: `${message}
+            <${AL_HOST}/agentlang/activeSuspension/${suspId}:approve|Approve>
+            <${AL_HOST}/agentlang/activeSuspension/${suspId}:reject|Reject>
+            `,
+            mrkdwn: true
+        })
     });
     return r.ts
 }
 
-export async function receive(threadId) {
-	const response = await waitForReply(threadId)
-	return response
+export async function receive(threadId, env) {
+    const suspData = env.lookupSuspensionUserData()
+    if (suspData) {
+        return suspData
+    }
+    const response = await waitForReply(threadId)
+    return response
 }
